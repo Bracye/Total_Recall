5914cd74b73d2b9dd81c74c37b7bcd0a8d097cd4
Ernest Bracy <ernestbracy1@gmail.com>
2023-06-15 10:09:49 -0400

    Hey you're not a dog

--- app.js
+++ app.js
@@ -4,42 +4,68 @@
 // firstVariable = 1;
 // let secondVariable = firstVariable;
 // secondVariable = "any string";
-
 // console.log(firstVariable); //1
+firstVar = "Hello World";
+firstVar = 22;
+secondVar = firstVar;
+secondVar = "Goodbye World";
+console.log(firstVar);
 
 // const yourName = "Ariel";
 // let hello = `Hello, my name is ${yourName}`
 // console.log(hello);
 
+const myName = "Ernest";
+let greetings = `Hello, my name is ${myName}.`;
+console.log(greetings);
+
 //BOOLEANS
 
-// const a = 4; 
-// const b = 53; 
-// const c = 57; 
-// const d = 16; 
+// const a = 4;
+// const b = 53;
+// const c = 57;
+// const d = 16;
 // const e = 'Kevin';
 
-// console.log(a < b); 
-// console.log(c > d); 
+// console.log(a < b);
+// console.log(c > d);
 // console.log('Name' == 'Name');
 
-//FOR THE NEXT TWO, USE ONLY && OR ||
-// console.log(true || false); 
-// console.log(false && false && false && false && false || true); 
-// console.log(false === false) 
-// console.log(e == 'Kevin'); 
-// console.log(a != b != c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations) 
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = "Kevin";
+
+console.log(a < b);
+console.log(c > d);
+console.log("Name" == "Name");
+
+// //FOR THE NEXT TWO, USE ONLY && OR ||
+// console.log(true || false);
+// console.log(false && false && false && false && false || true);
+// console.log(false === false)
+// console.log(e == 'Kevin');
+// console.log(a != b != c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
 // console.log(a == a || d); // note: the answer is a simple arithmetic equation, not something "weird" console.log(48 __ '48');
 
+console.log(true || false);
+console.log((false && false && false && false && false) || true);
+console.log(false === false);
+console.log(e == "Kevin");
+console.log((a != b) != c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+console.log(a == a || d); // note: the answer is a simple arithmetic equation, not something "weird" console.log(48 __ '48');
 
 //the Farm
-let animal = "cow";
+let animal = "Cow";
 
-const moo = (a) => {
-   return a.toLowerCase === "cow"? "mooooo" : "hey, that's not a cow"
-} 
+if (animal === "Dog") {
+  console.log("Bark Bark");
+} else {
+  console.log("Hey that's not a dog!");
+}
 
-console.log(moo(animal));
+console.log(bark(animal));
 //Driver's Ed
 
 //LOOPS
@@ -56,7 +82,6 @@ console.log(moo(animal));
 
 //OBJECTS
 
-
 // let longest = array.reduce( (firstEl, secondEl) => {
 //         //if the first el is greater than the second el return firstEl
 //         return firstEl.length > secondEl.length ? firstEl : secondEl;
@@ -64,5 +89,3 @@ console.log(moo(animal));
 // );
 
 // console.log(longest)
-
-
